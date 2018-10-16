9aecfe8e00f3856777218e8bdb0179762d655011
Basarat <basaratali@gmail.com>
2018-10-17 05:09:12 +1100

    well it works

--- cypress/integration/test.ts
+++ cypress/integration/test.ts
@@ -5,13 +5,13 @@ import * as example from '../../src/example';
 
 describe('should work', () => {
   it('should stub it once', () => {
-    cy.stub(example);
+    cy.stub(example, 'bar');
     foo('first');
     expect(example.bar).to.have.been.calledWith('first');
   });
 
   it('should stub it again', () => {
-    cy.stub(example);
+    cy.stub(example, 'bar');
     foo('second');
     expect(example.bar).to.have.been.calledWith('second');
   });
